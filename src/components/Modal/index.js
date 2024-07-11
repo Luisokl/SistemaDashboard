a1f70bdf8eda7da09e2e6273eaa6aa7baf3437b1
Luisokl <luis.melo.okl@outlook.com>
2024-07-10 22:16:49 -0300

    criando modal para mostrar os detalhes do atendimento registrado

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,9 +1,57 @@
 import './modal.css'
+import { FiX } from 'react-icons/fi'
 
-export default function Modal(){
-    return(
-        <div>
-            
+export default function Modal() {
+    return (
+        <div className='modal'>
+            <div className='container'>
+                <button className='btn-close'>
+                    <FiX size={25} color='#FFF' />
+                    Fechar
+                </button>
+
+                <main>
+                    <h2>Detalhes do Atendimento</h2>
+
+                    <div className='row'>
+                        <span>
+                            Cliente: <i>Fulano</i>
+                        </span>
+                        <span>
+                            Mecânico: <i>Ciclano</i>
+                        </span>
+                    </div>
+
+                    <div className='row'>
+                        <span>
+                            Automovel: <i>Fusion</i>
+                        </span>
+                        <span>
+                            Ano: <i>2037</i>
+                        </span>
+                    </div>
+
+                    <div className='row'>
+                        <span>
+                            Entrada: <i>27/06/2024</i>
+                        </span>
+                        <span>
+                            Orçamento: <i>1.500,00</i>
+                        </span>
+                    </div>
+                    <div className='row'>
+                        <span>
+                            Status: <i>Aberto</i>
+                        </span>
+                    </div>
+
+                    <>
+                        <h3>Complemento</h3>
+                        <p> Conteudo do complemento Conteudo do complemento Conteudo do complemento Conteudo do complemento Conteudo do complemento Conteudo do complementoConteudo do complemento Conteudo do complemento Conteudo do complemento vConteudo do complemento Conteudo do complemento</p>
+                    </>
+
+                </main>
+            </div>
         </div>
     )
 }
